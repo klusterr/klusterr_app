aa8018b03208d1f579c68eb0a1ea426983847f16
Richard Blackham <richard@klusterr.com>
2012-09-27 12:10:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KlusterrApp::Application.config.secret_token = '9879f1371865cc6745aed0cb22a2b1ffeed329696abb70941b3eb303bba9493ffe1eb7f4d27e9ed94ef8e2a18b508d5b767bd5f0782baf78f352d48b1b92328f'
